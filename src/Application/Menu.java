92ffeee3b35dc239881fa361e988483cb6edee59
Nawaraj Shahi <nawaraj.shahi@outlook.com>
2021-01-18 22:18:00 -0500

    updated menu

--- src/Application/Menu.java
+++ src/Application/Menu.java
@@ -1,37 +1,34 @@
 package Application;
-
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-
 import dao.OrganizationsDao;
-
 public class Menu {
 	
 	private OrganizationsDao orgDao = new OrganizationsDao();
 	
 	
 	Scanner input = new Scanner(System.in);
-	private List<String> tableOptions = Arrays.asList("Organizations", 
-													"Volunteers", 
+	private List<String> tableOptions = Arrays.asList("Organizations",
+													"Volunteers",
 													"Projects");
 	
-	private List<String> orgOptions = Arrays.asList("Dislay organizations", 
-													"Display an organization", 
-													"Create an organization",  
-													"Delete an organization", 
+	private List<String> orgOptions = Arrays.asList("Dislay organizations",
+													"Display an organization",
+													"Create an organization",
+													"Delete an organization",
 													"Return to main menu");
 	
-	private List<String> volOptions = Arrays.asList("Dislay volunteers", 
-													"Display a volunteer", 
-													"Create a volunteer",  
+	private List<String> volOptions = Arrays.asList("Dislay volunteers",
+													"Display a volunteer",
+													"Create a volunteer",
 													"Delete a volunteer",
 													"Return to main menu");
 	
-	private List<String> projOptions = Arrays.asList("Dislay projects", 
-													"Display a project", 
-													"Create a project",  
+	private List<String> projOptions = Arrays.asList("Dislay projects",
+													"Display a project",
+													"Create a project",
 													"Delete a project",
 													"Return to main menu");
 	
@@ -39,13 +36,12 @@ public class Menu {
 		String selection = "";
 		do {
 			System.out.println("\n**********Main-Menu**********");
-			System.out.println("----------------------------");
+			System.out.println("-----------------------------");
 			printMainMenu();
 			
 			selection = input.nextLine();
 			if(selection.equals("1")) {
 				printOrgMenu();
-				
 			}else if(selection.equals("2")) {
 				printVolMenu();
 			}else if(selection.equals("3")) {
@@ -60,33 +56,28 @@ public class Menu {
 	
 	//printMainMenu() implmeentation
 	private void printMainMenu() {
-		System.out.println("\nWhich table would you like to do maintenance to?");
-		for (int i = 0; i<tableOptions.size(); i++) {
-			System.out.println(i+1 + ") " + tableOptions.get(i));
-		}
+		printMenuPattern(tableOptions);
 	}
 	
 	//printOrgMenu() implementation
 	private void printOrgMenu() {
-		System.out.println("\nWhat maintenance would you like to perform?");
-		for (int i = 0; i<orgOptions.size(); i++) {
-			System.out.println(i+1 + ") " + orgOptions.get(i));
-		}
+		printMenuPattern(orgOptions);
 	}
 	
 	//printVolMenu() implmeentation
 	private void printVolMenu() {
-		System.out.println("\nWhat maintenance would you like to perform?");
-		for (int i = 0; i<volOptions.size(); i++) {
-			System.out.println(i+1 + ") " + volOptions.get(i));
-		}
+		printMenuPattern(volOptions);
 	}
 		
 	//printProjMenu() implementation
 	private void printProjMenu() {
-		System.out.println("\nWhat maintenance would you like to perform?");
-		for (int i = 0; i<projOptions.size(); i++) {
-			System.out.println(i+1 + ") " + projOptions.get(i));
+		printMenuPattern(projOptions);
+	}
+	
+	private void printMenuPattern(List<String> menu) {
+		System.out.println("\nWhich table would you like to do maintenance to?");
+		for (int i = 0; i<menu.size(); i++) {
+			System.out.println(i+1 + ") " + menu.get(i));
 		}
 	}
 }
